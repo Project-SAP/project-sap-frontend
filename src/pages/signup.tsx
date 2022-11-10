3426c075fe733f359bee52d5c65d1ec492b35de5
Samyak Piya <samyakpiya@gmail.com>
2022-11-10 15:01:39 -0500

    Add Username Field to Sign Up Form

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
-import Header from '../components/header'
-import Link from 'next/link'
+import Header from '../components/header';
+import Link from 'next/link';
 
 const SignUp: NextPage = () => {
     return (
@@ -15,28 +15,114 @@ const SignUp: NextPage = () => {
                             </h1>
                             <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                                    <input type="email" name="email" id="email" pattern="[A-Za-z0-9._%+-]+@uky.edu" title="Please enter a valid UKY email address." className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-dominant focus:border-dominant block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="linkblue@uky.edu" required />
+                                    <label
+                                        htmlFor="email"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        pattern="[A-Za-z0-9._%+-]+@uky.edu"
+                                        title="Please enter a valid UKY email address."
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-dominant focus:border-dominant block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="linkblue@uky.edu"
+                                        required
+                                    />
                                 </div>
                                 <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-dominant focus:border-dominant block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    <label
+                                        htmlFor="username"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >
+                                        Username
+                                    </label>
+                                    <input
+                                        type="username"
+                                        name="username"
+                                        id="username"
+                                        maxLength={30}
+                                        title="Please enter a username not more than 30 characters long."
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-dominant focus:border-dominant block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Wildcat"
+                                        required
+                                    />
                                 </div>
                                 <div>
-                                    <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                    <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-dominant focus:border-dominant block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    <label
+                                        htmlFor="password"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >
+                                        Password
+                                    </label>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        placeholder="••••••••••"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-dominant focus:border-dominant block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label
+                                        htmlFor="confirm-password"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >
+                                        Confirm password
+                                    </label>
+                                    <input
+                                        type="password"
+                                        name="confirm-password"
+                                        id="confirm-password"
+                                        placeholder="••••••••••"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-dominant focus:border-dominant block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required
+                                    />
                                 </div>
                                 <div className="flex items-start">
                                     <div className="flex items-center h-5">
-                                        <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-dominant dark:ring-offset-gray-800" required />
+                                        <input
+                                            id="terms"
+                                            aria-describedby="terms"
+                                            type="checkbox"
+                                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-dominant dark:ring-offset-gray-800"
+                                            required
+                                        />
                                     </div>
                                     <div className="ml-3 text-sm">
-                                        <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-dominant hover:underline dark:text-primary-500" href="#">Terms and Conditions.</a></label>
+                                        <label
+                                            htmlFor="terms"
+                                            className="font-light text-gray-500 dark:text-gray-300"
+                                        >
+                                            I accept the{' '}
+                                            <a
+                                                className="font-medium text-dominant hover:underline dark:text-primary-500"
+                                                href="#"
+                                            >
+                                                Terms and Conditions.
+                                            </a>
+                                        </label>
                                     </div>
                                 </div>
-                                <button type="submit" className="w-full text-white bg-dominant hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-dominant dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                                <button
+                                    type="submit"
+                                    className="w-full text-white bg-dominant hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-dominant dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                >
+                                    Create an account
+                                </button>
                                 <p className="text-sm text-center font-light text-gray-500 dark:text-gray-400">
-                                    Already have an account? <Link href="login"><a href="#" className="font-medium text-dominant hover:underline dark:text-primary-500">Sign in.</a></Link>
+                                    Already have an account?{' '}
+                                    <Link href="login">
+                                        <a
+                                            href="#"
+                                            className="font-medium text-dominant hover:underline dark:text-primary-500"
+                                        >
+                                            Sign in.
+                                        </a>
+                                    </Link>
                                 </p>
                             </form>
                         </div>
