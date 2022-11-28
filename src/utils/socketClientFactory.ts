a16d43f1b2a1477730a39310cb832e725df87956
Nicholas Reel <nicky.shadow99@gmail.com>
2022-11-27 23:09:49 -0500

    better docs for factory

--- src/utils/socketClientFactory.ts
+++ src/utils/socketClientFactory.ts
@@ -1,11 +1,17 @@
 import { io, Socket } from 'socket.io-client';
 
-// Ensure only a single socket connection is established per client
+/**
+ *  Ensure only a single socket connection is established per client
+ *  Required since pages will be reloaded regularly. Resulting in a new connection attempt.
+ */
 export default class SocketClientFactory {
     private static socketClient: Socket;
+
+    // Get path from envionment variable. Default to dev path if not found since localhost isn't a security concern.
+    // TODO: Would like to check environment first instead of defaulting to localhost. But it works for now.
     private static socketPath: string =
         process.env.NEXT_PUBLIC_SOCKET_PATH == undefined
-            ? ''
+            ? 'http://localhost:8080'
             : process.env.NEXT_PUBLIC_SOCKET_PATH;
 
     private constructor() {}
